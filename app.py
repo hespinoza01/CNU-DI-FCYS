4188a10bd24ffbf07a7503e098ddcc066b7e94a8
Harold Espinoza <haroldesptru@gmail.com>
2019-04-06 17:30:30 -0600

    Añadido script de servidor

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template as render
+
+
+app = Flask(__name__, static_folder='public', template_folder='views')
+
+
+@app.route('/')
+def index():
+    return render('index.html')
+
+
+if __name__ == '__main__':
+    app.run(
+        debug=True,
+        port=8000
+    )
