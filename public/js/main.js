a19eda3e8bd1634e3627ca7a2eb5283b61a1d077
Harold Espinoza <haroldesptru@gmail.com>
2019-04-08 13:23:21 -0600

    Añadido los estilos y eventos básicos para vada una de las páginas, también se creó la primer página de: Diplomados y cursos libres.

--- public/js/main.js
+++ public/js/main.js
@@ -1,34 +1,56 @@
 const TABS = {};
 
-    /*function loadTab(tabname){
-      let container = document.getElementById('content'),
-          searchInput = document.getElementById('searchInput'),
-          btnNew = document.getElementById('btnNew');
+function loadTab(tabname){
+  let container = document.getElementById('main');
 
-      container.innerHTML = TABS[tabname];
-      searchInput.placeholder = `Search ${tabname.split('-')[0]}`;
-      btnNew.textContent = `New ${(tabname === "books-tab") ? "book" : "category"}`;
-      btnNew.href = (tabname === "books-tab") ? "/book" : "/category";
-    }*/
+  container.innerHTML = TABS[tabname];
+  loadPagenavTabEvent();
+}
 
-    Array.from(document.querySelectorAll('.tab')).forEach(node => {
-      const TAB = document.getElementById(node.getAttribute('data-goto'));
+Array.from(document.querySelectorAll('.tab')).forEach(node => {
+  const TAB = document.getElementById(node.getAttribute('tab-target'));
 
-      if(TAB){
-        TABS[TAB.id] = TAB.innerHTML;
-        TAB.remove();
-      }
+  if(TAB){
+    TABS[TAB.id] = TAB.innerHTML;
+    TAB.remove();
+  }
 
-      // if(node.classList.contains('active')) loadTab(node.getAttribute('data-goto'));
+   if(node.classList.contains('active')) loadTab(node.getAttribute('tab-target'));
 
-      node.addEventListener('click', e => {
-        e.preventDefault();
+  node.addEventListener('click', e => {
+    e.preventDefault();
 
-        let prevTab = document.querySelector('.tab.active'),
-            nextTab = e.target;
+    let prevTab = document.querySelector('.tab.active'),
+        nextTab = e.target;
 
-        prevTab.classList.remove('active');
-        nextTab.classList.add('active');
-        // loadTab(nextTab.getAttribute('data-goto'));
-      });
+    prevTab.classList.remove('active');
+    nextTab.classList.add('active');
+    loadTab(nextTab.getAttribute('tab-target'));
+  });
+});
+
+
+// page-nav__tab event's
+function loadPagenavTabEvent() {
+  function changeBtnAddNewTextContent(textContent){
+    let btnAddNew = document.querySelector('.btnAddNew');
+
+    if(btnAddNew && textContent[textContent.length - 1] === "s") textContent = textContent.slice(0, -1);
+    btnAddNew.textContent = `Nuevo ${textContent}`;
+  }
+
+  Array.from(document.querySelectorAll('.page-nav__tab')).forEach(node => {
+
+    if(node.classList.contains('active')) changeBtnAddNewTextContent(node.textContent.toLowerCase());
+
+    node.addEventListener('click', e => {
+      let prevTab = document.querySelector('.page-nav__tab.active'),
+        nextTab = e.target;
+
+      prevTab.classList.remove('active');
+      nextTab.classList.add('active');
+
+      changeBtnAddNewTextContent(nextTab.textContent.toLowerCase());
     });
+  });
+}
