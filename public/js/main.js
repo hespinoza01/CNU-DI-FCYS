14d196073290213004863c5c86b057d368e3bc9a
Harold Espinoza <haroldesptru@gmail.com>
2019-04-09 05:32:44 -0600

    Completado tab de unidades académicas, pendiente: formulario de la secció

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,30 @@
 const TABS = {};
+const eventsSubtabs = {
+  'pageUnidadAcademica': function eventsUnidadAcademicaTab(){
+    const tableUA = document.getElementById('tableUA'),
+          tableTUA = document.getElementById('tableTUA'),
+          tableWrapper =  document.querySelector('.table-wrapper');
 
+    const tables = {
+      [tableUA.id]: tableUA.innerHTML,
+      [tableTUA.id]: tableTUA.innerHTML
+    };
+
+    tableUA.remove();
+    tableTUA.remove();
+
+    Array.from(document.querySelectorAll('.page-nav__tab')).forEach(tab => {
+      if(tab.classList.contains('active')) tableWrapper.innerHTML = tables[tab.getAttribute('table-target')];
+
+      tab.addEventListener('click', e => {
+        tableWrapper.innerHTML = tables[e.target.getAttribute('table-target')];
+        tableWrapper.style.width = (e.target.getAttribute('table-target') === 'tableUA') ? '60vw' : '';
+      });
+    });
+  }
+};
+
+// functions
 function loadTab(tabname){
   let container = document.getElementById('main');
 
@@ -7,6 +32,8 @@ function loadTab(tabname){
   loadPagenavTabEvent();
 }
 
+
+// Load tabs
 Array.from(document.querySelectorAll('.tab')).forEach(node => {
   const TAB = document.getElementById(node.getAttribute('tab-target'));
 
@@ -26,6 +53,7 @@ Array.from(document.querySelectorAll('.tab')).forEach(node => {
     prevTab.classList.remove('active');
     nextTab.classList.add('active');
     loadTab(nextTab.getAttribute('tab-target'));
+    eventsSubtabs[nextTab.getAttribute('tab-target')]();
   });
 });
 
